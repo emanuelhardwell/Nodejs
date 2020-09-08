862aeba6d48034a52b9bd3dabc5b94dd09ebcab2
emanuelhardwell <emanuelnoe16@gmail.com>
2020-09-07 22:28:55 -0500

    Exportar modulos

---
+++ domicilio.js
@@ -0,0 +1,9 @@
+/* 
+
+*/
+
+const domicilio = (calle, numDomicilio, colonia, estado, ciudad) => {
+  return `Tus datos son Calle: ${calle}, Núm. Domicilio: ${numDomicilio}, Colonia: ${colonia}, Estado: ${estado}, Ciudad: ${ciudad} `;
+};
+
+module.exports.domicilio = domicilio;
